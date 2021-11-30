8622dcf6a6432560cf2a32eac1bca9d65e55edd4
MugdhaD11 <93168425+MugdhaD11@users.noreply.github.com>
2021-11-29 17:21:51 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name="Mugdha "
+var last_name="Deshpande"
+var a=6
+var b=9
+console.log(first_name.concat(last_name));
+console.log("addition "+(a+b));
+console.log("multiplication "+(a*b));
+console.log("substraction "+(a-b));
+console.log("division "+(a/b));
